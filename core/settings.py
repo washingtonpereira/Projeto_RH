950967c6fdf25d9635add6d4e05a8f9e59963a0c
Washington Pereira <slash.o.teobaldo@gmail.com>
2025-04-01 16:42:59 -0300

    Criação da variável de ambiente

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xxwfvpb&wnbujadah1zmd1vy$2f$3iy_#%e43&&2av_6i(9e+@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
