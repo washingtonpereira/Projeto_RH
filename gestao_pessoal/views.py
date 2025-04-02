f9e24fa3675a1e7442623aaa181e502305b48b4c
Washington Pereira <slash.o.teobaldo@gmail.com>
2025-04-02 04:02:18 -0300

    Criada a view cadastro e listar

--- gestao_pessoal/views.py
+++ gestao_pessoal/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html/')
+    return render(request, 'home.html')
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+
+def listar(request):
+    return render(request, 'lista.hrml')
