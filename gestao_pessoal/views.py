6013ec0ab6a54bee8a7a871f650c4aa4ab8ca0cb
Washington Pereira <slash.o.teobaldo@gmail.com>
2025-04-18 00:43:26 -0300

    Renomeada o html e css

--- gestao_pessoal/views.py
+++ gestao_pessoal/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def cadastro(request):
     return render(request, 'cadastro.html')
